9f0099b346a343c30550d02dbf32bf8f3c5b33cd
svaduka <sainagaraju.vaduka@gmail.com>
2017-06-20 10:36:01 +0530

    Created WordCountDriver

---
+++ Batch080517/src/com/laboros/WordCountDriver.java
@@ -0,0 +1,20 @@
+package com.laboros;
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
+
+public class WordCountDriver extends Configured implements Tool {
+	
+	
+	public static void main(String[] args) 
+	{
+	}
+	
+
+	@Override
+	public int run(String[] arg0) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
