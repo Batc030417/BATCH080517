4574771459a846b000620100fe3ebe3748ca173f
svaduka <sainagaraju.vaduka@gmail.com>
2017-06-20 10:42:36 +0530

    Created Main Method

--- Batch080517/src/com/laboros/WordCountDriver.java
+++ Batch080517/src/com/laboros/WordCountDriver.java
@@ -1,13 +1,39 @@
 package com.laboros;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
 public class WordCountDriver extends Configured implements Tool {
 	
 	
 	public static void main(String[] args) 
 	{
+		
+		System.out.println("IN MAIN METHOD");
+	 //3 Steps
+		//step- 1 : Validations
+		if(args.length<2)
+		{
+			System.out.println("JAVA USAGE "+WordCountDriver.class.getName()+" /path/to/edgenode/local/file /path/to/hdfs/dest/dir");
+			return;
+		}
+		//step: 2
+		Configuration conf = new Configuration(Boolean.TRUE);
+		//step:3 
+		try {
+			int i=ToolRunner.run(conf, new WordCountDriver(), args);
+			if(i==0)
+			{
+				System.out.println("SUCCESS");
+			}else{
+				System.out.println("FAILURE");
+			}
+		} catch (Exception e) {
+			System.out.println("FAILURE");
+			e.printStackTrace();
+		}
 	}
 	
 
