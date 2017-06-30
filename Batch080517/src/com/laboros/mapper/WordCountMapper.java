135cd7b38a2f5f78a40c1158e407579055502d3c
svaduka <sainagaraju.vaduka@gmail.com>
2017-06-30 09:52:48 +0530

    Created Mapper

--- Batch080517/src/com/laboros/mapper/WordCountMapper.java
+++ Batch080517/src/com/laboros/mapper/WordCountMapper.java
@@ -1,20 +1,35 @@
 package com.laboros.mapper;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
-	
-	protected void setup(Context context) throws java.io.IOException, InterruptedException {
-	};
+public class WordCountMapper extends
+		Mapper<LongWritable, Text, Text, IntWritable> {
 
-	protected void map(LongWritable key,Text value,Context context)
+	protected void map(LongWritable key, Text value, Context context)
 			throws java.io.IOException, InterruptedException {
-	};
+		// key --0
+		// value -- DEER RIVER RIVER
 
-	protected void cleanup(Context context)
-			throws java.io.IOException, InterruptedException {
+		final String inputLine = value.toString();
+		if (StringUtils.isNotEmpty(inputLine)) 
+		{
+			final String wordSeperator = " ";
+			final String[] words = StringUtils.splitPreserveAllTokens(
+					inputLine, wordSeperator);
+			
+			Text input=new Text();
+			
+			final IntWritable ONE=new IntWritable(1);
+			
+			for (String word : words) 
+			{
+				input.set(word);
+				context.write(input, ONE);
+			}
+		}
 	};
 }
